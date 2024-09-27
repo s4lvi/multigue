0fc85cd3378eb92bef63d3dc8953eab5271ada1c
s4lvi <salvicomputers@gmail.com>
2024-09-26 22:33:52 -0400

    update

--- server/world.js
+++ server/world.js
@@ -36,18 +36,18 @@ class World {
   }
 
   loadOrCreateDungeon() {
-    const dungeonPath = path.join(__dirname, "dungeon.json");
-    if (fs.existsSync(dungeonPath)) {
-      console.log("Dungeon loaded from file.");
-      return JSON.parse(fs.readFileSync(dungeonPath, "utf-8"));
-    } else {
-      const dungeonWidth = 100;
-      const dungeonHeight = 100;
-      const dungeon = generateDungeon(dungeonWidth, dungeonHeight);
-      fs.writeFileSync(dungeonPath, JSON.stringify(dungeon));
-      console.log("Dungeon generated and saved to file.");
-      return dungeon;
-    }
+    // const dungeonPath = path.join(__dirname, "dungeon.json");
+    // if (fs.existsSync(dungeonPath)) {
+    //   console.log("Dungeon loaded from file.");
+    //   return JSON.parse(fs.readFileSync(dungeonPath, "utf-8"));
+    // } else {
+    const dungeonWidth = 150;
+    const dungeonHeight = 150;
+    const dungeon = generateDungeon(dungeonWidth, dungeonHeight);
+    fs.writeFileSync(dungeonPath, JSON.stringify(dungeon));
+    console.log("Dungeon generated and saved to file.");
+    return dungeon;
+    // }
   }
   // Load entity types from JSON files
   loadEntityTypes() {
