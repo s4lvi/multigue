70d770e48ee47997ab094bea2f44612f30b0f5d8
s4lvi <salvicomputers@gmail.com>
2024-10-11 17:05:57 -0400

    proc

--- multigue3/shared/constants/gameConstants.js
+++
@@ -1,6 +0,0 @@
-export const GAME_SETTINGS = {
-    PLAYER_SPEED: 5,
-    ENEMY_SPEED: 3,
-    CHAT_MAX_LENGTH: 200,
-    // Add more game constants here
-  };
--- multigue3/shared/utils/math.js
+++
@@ -1,6 +0,0 @@
-export const calculateDistance = (pointA, pointB) => {
-    const dx = pointA.x - pointB.x;
-    const dy = pointA.y - pointB.y;
-    const dz = pointA.z - pointB.z;
-    return Math.sqrt(dx * dx + dy * dy + dz * dz);
-  };
